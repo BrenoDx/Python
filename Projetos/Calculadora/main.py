5f73f5268b1c37bda0143e3ef00c1e9332d37173
breno <breno.alvesdx@gmail.com>
2026-02-24 21:26:55 -0300

    Feat: Adicionando opção de somar

--- Projetos/Calculadora/main.py
+++ Projetos/Calculadora/main.py
@@ -1,3 +1,4 @@
+from calculadora import somar,multiplicar,divide,subtrair
 loop = True
 
 print("======Calculadora======")
@@ -5,9 +6,15 @@ while loop:
     try:
         opcao = int(input("1 - Somar; 2 - Subtrair; 3 - divide; 4 - Multiplicar; 5 - Sair\n"))
 
+        if opcao == 1:
+            n1 = float(input("Informe o primeiro número:\n"))
+            n2 = float(input("Informe o segundo número:\n"))
+
+            print(f"Resultado é: {somar(n1,n2)}")
+
         if opcao == 5:
             print("Finalizando o programa")
             break
 
     except:
-        print("Opção Inválida!")
+        print("Entrada Inválida!")
