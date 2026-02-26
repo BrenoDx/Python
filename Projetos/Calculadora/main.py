dd493905580eea911339bf8e88076fbfa811d903
breno <breno.alvesdx@gmail.com>
2026-02-25 21:34:59 -0300

    Feat: Adicionando opção dividir

--- Projetos/Calculadora/main.py
+++ Projetos/Calculadora/main.py
@@ -12,8 +12,9 @@ while loop:
             print(f"Resultado é: {calculadora.somar(n1,n2)}")
         elif opcao == 2:
             print(f"Resultado é: {calculadora.subtrair(n1,n2)}")
-
-            
+        elif opcao == 3:
+            print(f"Resultado é: {calculadora.divide(n1,n2)}")
+          
         if opcao == 5:
             print("Finalizando o programa")
             break
