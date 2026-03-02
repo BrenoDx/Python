585d5c305f90a95115c29b186e7a31c5a6e9556e
breno <breno.alvesdx@gmail.com>
2026-03-02 20:09:56 -0300

    Style: Mensagem na opcao dividir

--- Projetos/Calculadora/calculadora.py
+++ Projetos/Calculadora/calculadora.py
@@ -6,6 +6,9 @@ class calculadora:
         return n1-n2
 
     def divide(n1,n2):
+        if n1 or n2 < 0:
+            print("Divisão por 0 resulta numa Indeterminação!")
+        
         return n1/n2
 
     def multiplicar(n1, n2):
