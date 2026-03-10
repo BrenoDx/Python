de87e5baab5dc3a566ac383b6e24ad53d679572c
breno <breno.alvesdx@gmail.com>
2026-03-10 19:16:43 -0300

    Feat: Enunciandos exerc intermediário parte 6

---
+++ Udemy/Seção 4 - Intermediário/Exer06.py
@@ -0,0 +1,17 @@
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos_produtos por deep copy
+
+produtos = [
+        {'nome': 'Produto 5', 'preco':10.00},
+        {'nome': 'Produto 1', 'preco':22.32},
+        {'nome': 'Produto 3', 'preco':10.11},
+        {'nome': 'Produto 2', 'preco':105.87},
+        {'nome': 'Produto 4', 'preco':69.90}
+]
+
+# Ordene os produtos por nome descrescente (do maior para o menor)
+# Gere Produtos_ordenados_por_nome por deep copy 
+
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere Produtos_ordenados_por_preco por deep copy
