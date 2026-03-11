61502d67458f39dc68bca61c7247ce43b7fb60fb
breno <breno.alvesdx@gmail.com>
2026-03-11 19:00:59 -0300

    Feat: Realizando terceira atv exerc intermediário parte 6

--- Udemy/Seção 4 - Intermediário/Exer06.py
+++ Udemy/Seção 4 - Intermediário/Exer06.py
@@ -9,11 +9,18 @@ produtos = [
         {'nome': 'Produto 2', 'preco':105.87},
         {'nome': 'Produto 4', 'preco':69.90}
 ]
+
 novos_produtos = [ {**p, 'preco': round(p['preco']*1.1, 2)} for p in copy.deepcopy(produtos) ]
 print(novos_produtos)
+
 # Ordene os produtos por nome descrescente (do maior para o menor)
 # Gere Produtos_ordenados_por_nome por deep copy 
+
 produtos_por_nome = sorted(copy.deepcopy(produtos), key=lambda p: p['nome'], reverse=True)
 print(produtos_por_nome)
+
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere Produtos_ordenados_por_preco por deep copy
+
+produtos_ordenados_por_preco = sorted(copy.deepcopy(produtos), key=lambda p: p['preco'])
+print(produtos_ordenados_por_preco)
