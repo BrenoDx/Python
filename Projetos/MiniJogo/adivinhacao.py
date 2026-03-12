c0c25c3016ddb12628eb2167979b3e4145fb90a8
breno <breno.alvesdx@gmail.com>
2026-03-11 21:24:52 -0300

    Feat: Projeto número secreto

---
+++ Projetos/MiniJogo/adivinhacao.py
@@ -0,0 +1,36 @@
+import os
+import random
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+print('==========Seja bem-vindo ao jogo de Advinhação==========')
+tentativas = 10
+numero_secreto = random.randint(1,10)
+print(numero_secreto)
+
+for i in range(9, -1, -1):
+    print(f'Você tem {tentativas} tentativas\nEscolha um número entre 0 a 20')
+    
+    escolha = input()
+    tentativas -= 1
+
+    if escolha.isdigit():
+        
+        if int(escolha) == numero_secreto:
+            limpar_tela()
+            print('Parabéns vc acertou o número')
+            print(f'Faltando {tentativas} tentativas!')
+            break
+        elif tentativas == 0:
+            limpar_tela()
+            print(f'Acabaram as tentativas que pena\nO número era: {numero_secreto}')
+        else:
+            print('Errou, tenta novamente!')
+    else:
+        print('Informe um número correto')
+
+        
+
+
+
