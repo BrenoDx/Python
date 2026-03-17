682247290f7383af6cfcee1155c9964bfeec78e8
breno <breno.alvesdx@gmail.com>
2026-03-16 21:29:30 -0300

    Feat: Adicionando todas combinações do jogo

--- Projetos/MiniJogo/jokenpo.py
+++ Projetos/MiniJogo/jokenpo.py
@@ -18,6 +18,14 @@ while jogar:
     if escolha_usuario not in mao:
         print('Essa forma não faz parte do jogo!')
 
-    if escolha_usuario == 'pedra' and escolha_oponente == 'tesoura':
-        print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
-    
+    if escolha_usuario == escolha_oponente:
+        print('Empate, ambos jogaram a mesma forma')
+    else:
+        if escolha_usuario == 'pedra' and escolha_oponente == 'tesoura':
+            print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
+        elif escolha_usuario == 'papel' and escolha_oponente == 'pedra':
+            print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
+        elif escolha_usuario == 'tesoura' and escolha_oponente == 'papel':
+            print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
+        else:
+            print(f'Você perdeu, oponente jogou {escolha_oponente}')
