a2668a077f7c5b1dd2ed13d68b50ebede22c844e
breno <breno.alvesdx@gmail.com>
2026-03-17 19:51:29 -0300

    Feat: Adicionando opção de parar o jogo além do contadores de resultado

--- Projetos/MiniJogo/jokenpo.py
+++ Projetos/MiniJogo/jokenpo.py
@@ -1,9 +1,11 @@
-
-
 import random
 
 
+vitoria = 0
+derrota = 0
+empate = 0
 jogar = True
+escolha = ''
 mao = [
     'pedra','papel', 'tesoura'
 ]
@@ -20,12 +22,24 @@ while jogar:
 
     if escolha_usuario == escolha_oponente:
         print('Empate, ambos jogaram a mesma forma')
+        empate +=1
     else:
         if escolha_usuario == 'pedra' and escolha_oponente == 'tesoura':
             print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
+            vitoria +=1
         elif escolha_usuario == 'papel' and escolha_oponente == 'pedra':
             print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
+            vitoria +=1
         elif escolha_usuario == 'tesoura' and escolha_oponente == 'papel':
             print(f'Parabéns você ganhou, oponente jogou {escolha_oponente}')
+            vitoria +=1
         else:
             print(f'Você perdeu, oponente jogou {escolha_oponente}')
+            derrota +=1
+
+    escolha = input('Deseja jogar novamente? [S/N]')
+    if escolha.lower() == 'n':
+        jogar = False
+
+print('==========Resultado Final ==========')
+print(f'Você teve {vitoria} vitória[s], {empate} empate[s] e {derrota} derrota[s] ')
